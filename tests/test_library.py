d476474d5cbdf47c02bf16d65a2c5c2354631394
Vasiliy <vaakoo.tr@gmail.com>
2025-12-09 15:38:23 +0300

    добавил тест для удаления пользователя

---
+++ tests/test_library.py
@@ -0,0 +1,17 @@
+
+
+def test_register_user_basic():
+    libr = Library("Моя библиотека")
+    usr = User(id=1, name="Джони")
+    libr.register_user(usr)
+    assert usr in libr.users
+
+def test_remove_user_basic():
+    libr = Library("Моя библиотека")
+    usr1 = User(id=2, name="Dog")
+    usr2 = User(id=3, name="Jack")
+    libr.register_user(usr1)
+    libr.register_user(usr2)
+    libr.remove_user(2)
+    assert usr1 not in libr.users
+    assert usr2 in libr.users
