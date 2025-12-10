57ac9e50c20f72ff924a5c190273619ae177269e
Vasiliy <vaakoo.tr@gmail.com>
2025-12-10 16:59:12 +0300

    update

--- library/user.py
+++ library/user.py
@@ -1,14 +1,9 @@
 from dataclasses import dataclass
 from typing import List
+from library.borrow_record import BorrowRecord
 
 @dataclass
 class User:
     id: int
     name: str
     borrowed_books: List[BorrowRecord] = []
-
-    def borrow_book(self, record: BorrowRecod):
-        raise NotImplementedError("тут должен быть метод")
-
-    def return_book(book_id: int):
-        raise NotImplementedError("тут должен быть метод")
