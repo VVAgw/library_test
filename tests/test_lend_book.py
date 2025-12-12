1e38b945df9fd06021d211062c526df3d46335f7
Vasiliy <vaakoo.tr@gmail.com>
2025-12-12 16:49:43 +0300

    добавил тест выдачи книги

--- tests/test_lend_book.py
+++ tests/test_lend_book.py
@@ -1,12 +1,13 @@
 from datetime import date
 from library.library import Library
 from library.user import User
+from library.borrow_record import BorrowRecord
 from library.book import Book
 
 def test_lend_book_basic():
     libr = Library("My library")
     book = Book(id=3, autor="WA", title="Test")
-    usr = User(id=3, name="vva")
+    usr = User(user_id=3, user_name="vva")
 
     libr.register_user(usr)
     libr.add_book(book)
@@ -14,9 +15,9 @@ def test_lend_book_basic():
     due = date(2025,10,10)
     libr.lend_book(book_id=3, user_id=3, due_date=due)
 
-    assert book.is_avaliable is False
+    assert book.is_available is False
     assert len(usr.borrowed_books) == 1
-    record = user.borrowed_books[0]
+    record = usr.borrowed_books[0]
     assert record.book_id == 3
     assert record.user_id == 3
     assert record.due_date == due
