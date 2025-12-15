8d89a3cb4dcb1bc5481bd48adad19071a10847a2
Vasiliy <vaakoo.tr@gmail.com>
2025-12-15 16:53:13 +0300

    добавил тест просточки возвращения книги

--- tests/test_return_book.py
+++ tests/test_return_book.py
@@ -1,4 +1,4 @@
-from datetime import date
+from datetime import date, timedelta
 from library.borrow_record import BorrowRecord
 from library.user import User
 from library.book import Book
@@ -13,6 +13,21 @@ def test_return_book_bosic():
 
     due = date(2025, 1, 1)
     libr.lend_book(book_id=4, user_id=3, due_date=due)
+    libr.return_book(book_id=4)
+
+    assert book.is_available is True
+    assert book.active_record is None
+    assert len(user.borrowed_books) == 0
+
+def test_return_overdue_book():
+    libr = Library("MyLibr")
+    user = User(user_id=3, user_name="WA")
+    book = Book(id=4, title="World?", autor="Psh")
+    libr.add_book(book)
+    libr.register_user(user)
+
+    overdue_date = date.today() - timedelta(days=5)
+    libr.lend_book(book_id=4, user_id=3, due_date=overdue_date)
     libr.return_book(book_id=4)
 
     assert book.is_available is True
